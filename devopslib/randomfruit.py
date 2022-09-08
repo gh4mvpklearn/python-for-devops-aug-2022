d2b6709eddbd147785405520c0ecf0d9dd3f9df4
mvpklearn <mvpklearn@gmail.com>
2022-09-08 08:19:33 +0000

    Added randomfruit example

---
+++ devopslib/randomfruit.py
@@ -0,0 +1,5 @@
+from random import choices
+
+def fruit():
+    fruits=["Apple", "Mango", "Kiwi"]
+    return choices(fruits)[0]
--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+from devopslib.randomfruit import fruit
 print("Hello world")
 Name = "John"
 print(f"Hello {Name} welcome")
+print(fruit())
